94e3e42966143406cc916b23964a715be525276b
prometheus-eropro <assistentes.prometheus@gmail.com>
2026-02-14 00:24:09 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "autocontrole-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./favicon.png",
+        "./logo.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => {
+      return resp || fetch(e.request);
+    })
+  );
+});
