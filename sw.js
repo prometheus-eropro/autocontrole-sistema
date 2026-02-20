da06b41dd23cef6b97232810f6f586b7063187cb
prometheus-eropro <assistentes.prometheus@gmail.com>
2026-02-20 09:48:51 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,35 @@
-const CACHE_NAME = "autocontrole-v1";
+const CACHE_NAME = "autocontrole-v2"; // MUDE O NÚMERO SEMPRE QUE ATUALIZAR
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./favicon.png",
-        "./logo.png"
-      ]);
-    })
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => {
-      return resp || fetch(e.request);
-    })
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
